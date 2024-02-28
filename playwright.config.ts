c93f228f8f23d50b57a498894ac844b27d5b9468
vhcsilva <vhcsilvaa@gmail.com>
2024-02-28 11:58:22 -0300

    use one config in the project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "@playwright/test";
 export default defineConfig({
   timeout: 300000,
+  workers: 1,
   use: {
     permissions: ['clipboard-read', 'clipboard-write'],
     screenshot: 'only-on-failure',
   },
+  fullyParallel: false,
   webServer: [
     // {
     //   command: "npm run preview --port 8080",
